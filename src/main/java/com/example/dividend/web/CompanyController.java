7e57dd82d7de041e6c4b794aa325edfda7dccecf
geun02k <geun02k@gmail.com>
2024-12-11 20:49:47 +0900

    배당금저장API - 회사 및 배당금 정보 저장 api controller 수정

--- src/main/java/com/example/dividend/web/CompanyController.java
+++ src/main/java/com/example/dividend/web/CompanyController.java
@@ -1,11 +1,18 @@
 package com.example.dividend.web;
 
+import com.example.dividend.model.Company;
+import com.example.dividend.service.CompanyService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/company")
 public class CompanyController {
+
+    private final CompanyService companyService;
 
     // 회사검색 자동완성
     @GetMapping("/autocomplete")
@@ -19,9 +26,18 @@ public class CompanyController {
         return null;
     }
 
+    // 회사 및 배당금 정보 저장
     @PostMapping
-    public ResponseEntity<?> addCompany() {
-        return null;
+    public ResponseEntity<?> addCompany(@RequestBody Company request) {
+        String ticker = request.getTicker().trim();
+        if(ObjectUtils.isEmpty(ticker)) {
+            throw new RuntimeException("Ticker is empty.");
+        }
+
+        Company company = this.companyService.save(ticker);
+
+        // 회사정보(company) 반환
+        return ResponseEntity.ok(company);
     }
 
     @DeleteMapping
