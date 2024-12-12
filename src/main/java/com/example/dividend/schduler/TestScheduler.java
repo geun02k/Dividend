fdb3ecfdb293fbeb5aa29affef1122700bbb9070
geun02k <geun02k@gmail.com>
2024-12-12 16:26:43 +0900

    스케줄러 - 스케줄링 테스트코드 주석처리

--- src/main/java/com/example/dividend/schduler/TestScheduler.java
+++ src/main/java/com/example/dividend/schduler/TestScheduler.java
@@ -1,13 +1,13 @@
 package com.example.dividend.schduler;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TestScheduler {
 
-    @Scheduled(cron="0/5 * * * * *")
-    public void test() {
-        System.out.println("now ->" + System.currentTimeMillis());
-    }
+//    @Scheduled(cron="0/5 * * * * *")
+//    public void test() {
+//        System.out.println("now ->" + System.currentTimeMillis());
+//    }
+
 }
