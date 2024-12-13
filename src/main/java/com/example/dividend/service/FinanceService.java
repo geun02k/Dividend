62e8ee4ed6b17c557dd3e93612e0c0447f5d3761
geun02k <geun02k@gmail.com>
2024-12-13 21:48:56 +0900

    Redis 캐싱 - 배당금조회 service 데이터 캐시서버에 저장

--- src/main/java/com/example/dividend/service/FinanceService.java
+++ src/main/java/com/example/dividend/service/FinanceService.java
@@ -8,11 +8,14 @@ import com.example.dividend.persist.DividendRepository;
 import com.example.dividend.persist.entity.CompanyEntity;
 import com.example.dividend.persist.entity.DividendEntity;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class FinanceService {
@@ -21,7 +24,14 @@ public class FinanceService {
     private final DividendRepository dividendRepository;
 
     /** 배당금내역 조회 */
+    // @Cacheable
+    // : 해당 key값에 대한 Cache 데이터가 Redis Cache에 존재하지 않을 경우 -> 아래의 서비스를 실행
+    //   데이터가 존재하는 경우 아래의 서비스 미실행 -> Cahce에 있는 데이터 반환.
+    @Cacheable(key = "#companyName", value = "finance")
     public ScrapedResult getDividendByCompanyName(String companyName) {
+        // Redis Cache 서버에서 데이터를 가져올 때는 해당 로그 출력되지 않음.
+        log.info("search company -> " + companyName);
+
         // 1. 회사명 기준 회사정보 조회
         CompanyEntity companyEntity = this.companyRepository.findByName(companyName)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다."));
