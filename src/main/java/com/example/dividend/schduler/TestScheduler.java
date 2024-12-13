b58dc9dcbe38354ad1755ff268643435cb57b37f
geun02k <geun02k@gmail.com>
2024-12-13 21:43:17 +0900

    스케줄러&스레드풀 - 스테드풀 생성으로 여러 스케줄 동시처리 수행 테스트 주석처리

--- src/main/java/com/example/dividend/schduler/TestScheduler.java
+++ src/main/java/com/example/dividend/schduler/TestScheduler.java
@@ -13,14 +13,14 @@ public class TestScheduler {
 //        System.out.println("now ->" + System.currentTimeMillis());
 //    }
 
-    @Scheduled(fixedDelay = 1000)
-    public void test1() throws InterruptedException {
-        Thread.sleep(10000);
-        System.out.println(Thread.currentThread().getName() + " -> 테스트1 : " + LocalDateTime.now());
-    }
-
-    @Scheduled(fixedDelay = 1000)
-    public void test2() {
-        System.out.println(Thread.currentThread().getName() + " -> 테스트2 : " + LocalDateTime.now());
-    }
+//    @Scheduled(fixedDelay = 1000)
+//    public void test1() throws InterruptedException {
+//        Thread.sleep(10000);
+//        System.out.println(Thread.currentThread().getName() + " -> 테스트1 : " + LocalDateTime.now());
+//    }
+//
+//    @Scheduled(fixedDelay = 1000)
+//    public void test2() {
+//        System.out.println(Thread.currentThread().getName() + " -> 테스트2 : " + LocalDateTime.now());
+//    }
 }
