86c558a48923162575666755795c354f9196fdb0
geun02k <geun02k@gmail.com>
2024-12-13 21:50:53 +0900

    Redis 캐싱 - 캐시서버에 저장불가 오류해결 (LocalDateTime 타입 직렬화 불가 에러해결)

--- src/main/java/com/example/dividend/model/Dividend.java
+++ src/main/java/com/example/dividend/model/Dividend.java
@@ -1,5 +1,9 @@
 package com.example.dividend.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,6 +13,8 @@ import java.time.LocalDateTime;
 @Builder
 public class Dividend {
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDateTime date;
     private String dividend;
 }
